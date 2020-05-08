b3ef4d5ee2cdd14310a62fb04b7139adf1c695d4
merve nur yazanel <54467555+mervenuryazanel@users.noreply.github.com>
2020-05-08 23:38:11 +0300

    Create iletisim.php

---
+++ iletisim.php
@@ -0,0 +1,22 @@
+<html>
+
+    <body>
+
+ 
+
+        Ad Soyad: <?php echo $_POST["fadsoyad"]; ?><br>
+        Kullanıcı Adı: <?php echo $_POST["fkadi"]; ?><br>
+        Email: <?php echo $_POST["femail"]; ?><br>
+        <?php echo $_POST["fsifre"]; ?><br>
+        Telefon Numarası: <?php echo $_POST["ftelNo"]; ?><br>
+        Doğum Tarihi: <?php echo $_POST["fdogumTarihi"]; ?><br>
+        Cinsiyet: <?php echo $_POST["fcinsiyet"]; ?><br>
+        Seçenek: <?php echo $_POST["fsecenek"]; ?><br>
+        Yorum: <?php echo $_POST["fyorum"]; ?><br>
+        <?php echo $_POST["fcheck"]; ?><br>
+
+ 
+
+    </body>
+
+</html>
